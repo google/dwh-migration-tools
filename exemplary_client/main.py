000e6b977e9e2f8c3f1bd3828c8afb7abd0f1c8d
Xia Shang <xshang@google.com>
2022-05-13 09:37:10 -0700

    Add a python exemplary client tool.

---
+++ exemplary_client/main.py
@@ -0,0 +1,45 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import batch_sql_translator
+
+import argparse
+from config_parser import ConfigParser
+from sql_processor import MacrosMapParser
+
+parser = argparse.ArgumentParser(description='Config the Batch Sql translation tool.')
+parser.add_argument('-m', '--macro_map', type=str,
+                    help='Path to the macro map yaml file. If specified, the program will pre-process '
+                         'all the input query files by replacing the macros with corresponding '
+                         'string values according to the macro map definition. After translation, '
+                         'the program will reverse the macro substitution for the output files in a '
+                         'post-processing step.')
+args = parser.parse_args()
+
+
+def start_translation():
+    """Starts a batch translation job.
+    """
+    print("Reading translation config file from config.yaml")
+    config = ConfigParser().parse_config()
+    if args.macro_map:
+        print('Reading macros replacement map from %s' % args.macro_map)
+        config.macro_maps = MacrosMapParser(args.macro_map).parse()
+    translator = batch_sql_translator.BatchSqlTranslator(config)
+    translator.start_translation()
+
+
+if __name__ == '__main__':
+    start_translation()
