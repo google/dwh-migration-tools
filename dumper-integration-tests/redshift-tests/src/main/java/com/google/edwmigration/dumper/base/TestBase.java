03e12dfffe5a524a3d5cbb6a52cdd989cf2bf978
Danila Morokov <morokov@google.com>
2022-05-25 13:50:29 +0000

    Integration test for CVV_COLUMNS table
    Kokoro build script invokes Dumper and Dumper extracts metadata to a local folder on a build worker,
    the path to a folder will be stored in EXPORT_PATH env var.
    After that, integration tests will be run and pointed to EXPORT_PATH to read CSV files from there.
    The logic of integration tests is to make an SQL request to DB, extract the data and save them to memory,
    then read CSV files, deserialize it, and then compare with the extraction from DB that the both data sets are
    field-to-field equal.

---
+++ dumper-integration-tests/redshift-tests/src/main/java/com/google/edwmigration/dumper/base/TestBase.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.base;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+
+import com.google.common.collect.LinkedHashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Base class with general values for all TestNG test suites */
+public abstract class TestBase {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestBase.class);
+
+  /**
+   * @param dbMultiset List of extracted from DB items.
+   * @param csvMultiset List of uploaded from Avro items.
+   *     <p>This custom method exists due to Java out of heap memory error in
+   *     Truth.assertThat(dbMultiset).containsExactlyElementsIn(csvMultiset); It probably happens
+   *     because .containsExactlyElementsIn() tries to print out not only the diff, let's say 1
+   *     element, but entire collections.
+   */
+  public static void assertDbCsvDataEqual(
+      LinkedHashMultiset<?> dbMultiset, LinkedHashMultiset<?> csvMultiset) {
+    Multiset<?> dbReducedOnCsv = Multisets.difference(dbMultiset, csvMultiset);
+    Multiset<?> csvReducedOnDb = Multisets.difference(csvMultiset, dbMultiset);
+
+    String dbDiffEntries =
+        dbReducedOnCsv.stream().map(e -> e.toString() + "\n").collect(toList()).toString();
+    String csvDiffEntries =
+        csvReducedOnDb.stream().map(e -> e.toString() + "\n").collect(toList()).toString();
+
+    if (dbReducedOnCsv.isEmpty() && csvReducedOnDb.isEmpty()) {
+      LOGGER.info("DB view and CSV file are equal");
+    } else if (!dbReducedOnCsv.isEmpty() && !csvReducedOnDb.isEmpty()) {
+      Assert.fail(
+          format(
+              "DB view and CSV file have mutually exclusive row(s)%n"
+                  + "DB view has %d different row(s): %s%n"
+                  + "CSV file has %d different row(s): %s",
+              dbReducedOnCsv.size(), dbDiffEntries, csvReducedOnDb.size(), csvDiffEntries));
+    } else if (!dbReducedOnCsv.isEmpty()) {
+      Assert.fail(format("DB view has %d extra row(s):%n%s", dbReducedOnCsv.size(), dbDiffEntries));
+    } else if (!csvReducedOnDb.isEmpty()) {
+      Assert.fail(
+          format("CSV file has %d extra row(s):%n%s", csvReducedOnDb.size(), csvDiffEntries));
+    }
+  }
+}
