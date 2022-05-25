7570429dd3fafd3fafbe7f2f58ad7641bfb5fed3
Danila Morokov <morokov@google.com>
2022-05-25 13:50:29 +0000

    CVV_COLUMNS table integration test
    1. Added JdbcUtils and methods
    2. Added svv_columns.sql for retrieving table's data
    3. Added SvvColumnsTest test
    4. Added SvvColumnsRow POJO object
    5. Added CsvUtil and methods
    6. Added Gradle reporting plugin

--- dumper-integration-tests/redshift-tests/src/main/java/com/google/edwmigration/dumper/jdbc/JdbcUtil.java
+++ dumper-integration-tests/redshift-tests/src/main/java/com/google/edwmigration/dumper/jdbc/JdbcUtil.java
@@ -15,11 +15,15 @@
  */
 package com.google.edwmigration.dumper.jdbc;
 
+import static com.google.edwmigration.dumper.base.TestConstants.TRAILING_SPACES_REGEX;
+
+import java.math.BigDecimal;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * A helper class for checking Null values returned by executing SELECT request against a database.
@@ -35,29 +39,67 @@ public final class JdbcUtil {
    */
   public static String getStringNotNull(ResultSet rs, String column) throws SQLException {
     String string = rs.getString(column);
-    return rs.wasNull() ? "" : string;
+    return rs.wasNull() ? "" : TRAILING_SPACES_REGEX.matcher(string).replaceFirst("");
   }
 
   /**
    * @param rs A row with SELECT results.
-   * @param columnIndex Database column index.
-   * @return String or an empty string if null.
+   * @param column Database column name.
+   * @return int or 0 if null.
    */
-  public static String getStringNotNull(ResultSet rs, int columnIndex) throws SQLException {
-    String string = rs.getString(columnIndex);
-    return rs.wasNull() ? "" : string;
+  public static int getIntNotNull(ResultSet rs, String column) throws SQLException {
+    int intValue = rs.getInt(column);
+    return rs.wasNull() ? 0 : intValue;
   }
 
   /**
-   * @param rsmd Metadata of the executed SQL query.
-   * @return List of column names.
-   * @throws SQLException
+   * @param rs A row with SELECT results.
+   * @param column Database column name.
+   * @return long or 0L if null.
    */
-  public static List<String> getDbColumnNames(ResultSetMetaData rsmd) throws SQLException {
-    List<String> columnNames = new ArrayList<>();
-    for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-      columnNames.add(rsmd.getColumnName(i));
+  public static long getLongNotNull(ResultSet rs, String column) throws SQLException {
+    long longValue = rs.getLong(column);
+    return rs.wasNull() ? 0L : longValue;
+  }
+
+  /**
+   * @param rs A row with SELECT results.
+   * @param column Database column name.
+   * @return byte[] or empty byte[] if null.
+   */
+  public static byte[] getBytesNotNull(ResultSet rs, String column) throws SQLException {
+    try {
+      byte[] bytesValue = rs.getBytes(column);
+      return rs.wasNull() ? new byte[0] : bytesValue;
+    } catch (SQLException e) {
+      BigDecimal bigDecimal = rs.getBigDecimal(column);
+      return rs.wasNull() ? new byte[0] : bigDecimal.toBigInteger().toByteArray();
     }
-    return columnNames;
+  }
+
+  /**
+   * @param rs A row with SELECT results.
+   * @param column Database column name.
+   * @return double or 0.0 if null.
+   */
+  public static double getDoubleNotNull(ResultSet rs, String column) throws SQLException {
+    double doubleValue = rs.getDouble(column);
+    return rs.wasNull() ? 0.0 : doubleValue;
+  }
+
+  /**
+   * @param rs A row with SELECT results.
+   * @param column Database column name.
+   * @return long or 0L if null.
+   */
+  public static long getTimestampNotNull(ResultSet rs, String column) throws SQLException {
+    Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    Timestamp timestamp = rs.getTimestamp(column, cal);
+    if (rs.wasNull()) {
+      return 0L;
+    }
+    return Timestamp.from(
+            ZonedDateTime.of(timestamp.toLocalDateTime(), cal.getTimeZone().toZoneId()).toInstant())
+        .getTime();
   }
 }
