73d6337b6d7a9921173eb57de055d1c3abacae46
Christian Haselbach <haselbach@google.com>
2022-08-16 04:50:16 -0700

    Add query metrics to Redshift raw log connector (#77)
    Dump the view STL_QUERY_METRICS to querymetrics_... in the Redshift raw logs connector when the --assessment flag is set.

--- dumper/lib-dumper-spi/src/main/java/com/google/edwmigration/dumper/plugin/lib/dumper/spi/RedshiftRawLogsDumpFormat.java
+++ dumper/lib-dumper-spi/src/main/java/com/google/edwmigration/dumper/plugin/lib/dumper/spi/RedshiftRawLogsDumpFormat.java
@@ -71,6 +71,23 @@ public interface RedshiftRawLogsDumpFormat {
         }
     }
 
+    public static interface QueryMetricsHistory {
+
+        public static final String ZIP_ENTRY_PREFIX = "querymetrics_";
+
+        public static enum Header {
+            userid, service_class, query, segment, step_type, starttime, slices, max_rows, rows,
+            max_cpu_time, cpu_time, max_blocks_read, blocks_read, max_run_time, run_time,
+            max_blocks_to_disk, blocks_to_disk, step, max_query_scan_size, query_scan_size,
+            query_priority, query_queue_time, service_class_name;
+
+        }
+
+        public static boolean isZipEntryName(@Nonnull String name) {
+            return name.startsWith(ZIP_ENTRY_PREFIX) && name.endsWith(ZIP_ENTRY_SUFFIX);
+        }
+    }
+
     interface QueryQueueInfo {
         String ZIP_ENTRY_PREFIX = "query_queue_info_";
 
