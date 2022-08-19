c919fa19c388632c8b6bfc5576749601aa06a3f0
Roman Fursov <fursovroman95@gmail.com>
2022-08-19 11:55:18 +0200

    Add "--assessment" flag, order Redshift raw logs if it is enabled (#81)
    * Add "--assessment" flag, order Redshift raw logs if it is enabled

---
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/annotations/RespectsArgumentAssessment.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Google LLC
+ * Copyright 2013-2021 CompilerWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.application.dumper.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import com.google.edwmigration.dumper.application.dumper.ConnectorArguments;
+
+/**
+ *
+ * @author frsv
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@RespectsInput(order = 2000, arg = ConnectorArguments.OPT_ASSESSMENT, description = RespectsArgumentAssessment.DESCRIPTION, required = RespectsArgumentAssessment.REQUIRED)
+public @interface RespectsArgumentAssessment {
+
+    String DESCRIPTION = "Whether to dump additional data for Migration Assessment in the service compatible format.";
+    String REQUIRED = "when dumping for BigQuery Migration Assessment service";
+}
