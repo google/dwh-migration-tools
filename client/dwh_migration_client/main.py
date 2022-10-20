b644fc996759ea2193a843d02da41010103839dd
Brian Schott <briancs@google.com>
2022-10-20 23:25:34 +0000

    New macro processing code for the Python client (#109)
    * New macro processing code for the Python client
    
    * Formatting fixes
    
    * Code cleanup
    
    * Allow more than one macro processor to handle a file

--- client/dwh_migration_client/main.py
+++ client/dwh_migration_client/main.py
@@ -23,8 +23,8 @@ from marshmallow import ValidationError
 
 from dwh_migration_client import batch_sql_translator
 from dwh_migration_client.config import parse as parse_config
+from dwh_migration_client.custom_macros import custom_macros
 from dwh_migration_client.gcloud_auth_helper import validate_gcloud_auth_settings
-from dwh_migration_client.macro_processor import MacroProcessor
 from dwh_migration_client.object_name_mapping import parse as parse_object_name_mapping
 from dwh_migration_client.validation import (
     validated_directory,
@@ -52,7 +52,7 @@ def start_translation(args: argparse.Namespace) -> None:
 
     if args.macros:
         try:
-            preprocessor = MacroProcessor(args)
+            preprocessor = custom_macros(args)
         except ValidationError:
             sys.exit(1)
     else:
