fc44610319267838dde825941b0cdaa3fd13376e
Roman Fursov <rfursov@google.com>
2022-11-21 16:48:54 +0100

    Add CompilerWorks copyright as it breaks spotless check

--- dumper/lib-ext-hive-metastore/src/main/java/com/google/edwmigration/dumper/ext/hive/metastore/Database.java
+++ dumper/lib-ext-hive-metastore/src/main/java/com/google/edwmigration/dumper/ext/hive/metastore/Database.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2022 Google LLC
+ * Copyright 2013-2021 CompilerWorks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
