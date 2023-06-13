c37f49d11dc95361ac92585b269ccdc6fff2793b
Ishmum Jawad Khan <ishmumkhan@google.com>
2023-06-13 17:17:37 +0200

    Change default snowflake data retrieval methods. (#215)

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/snowflake/SnowflakeAccountUsageMetadataConnector.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/snowflake/SnowflakeAccountUsageMetadataConnector.java
@@ -17,9 +17,9 @@
 package com.google.edwmigration.dumper.application.dumper.connector.snowflake;
 
 import com.google.auto.service.AutoService;
+import com.google.edwmigration.dumper.application.dumper.ConnectorArguments;
 import com.google.edwmigration.dumper.application.dumper.connector.Connector;
 import com.google.edwmigration.dumper.application.dumper.connector.MetadataConnector;
-import com.google.edwmigration.dumper.application.dumper.connector.snowflake.SnowflakeMetadataConnector.TaskVariant;
 import com.google.edwmigration.dumper.application.dumper.task.JdbcSelectTask;
 import com.google.edwmigration.dumper.application.dumper.task.Task;
 import com.google.edwmigration.dumper.plugin.ext.jdk.annotation.Description;
@@ -40,7 +40,8 @@ public class SnowflakeAccountUsageMetadataConnector extends SnowflakeMetadataCon
       Class<? extends Enum<?>> header,
       String format,
       TaskVariant is_task,
-      TaskVariant au_task) {
+      TaskVariant au_task,
+      ConnectorArguments arguments) {
     Task<?> t0 =
         new JdbcSelectTask(
                 au_task.zipEntryName,
