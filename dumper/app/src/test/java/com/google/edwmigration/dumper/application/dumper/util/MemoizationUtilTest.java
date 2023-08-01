b68b58282e975b1d534f71b7e0ffe17e15c6fed9
Christian Haselbach <haselbach@google.com>
2023-08-01 08:45:21 -0700

    Add memoization to the driver retriever (#249)
    The cloud extractor can do multiple dumps and will likely require the
    same driver for all runs (e.g. metadata and logs for the same DB). Thus,
    the driver for it only needs to be downloaded once.

---
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/util/MemoizationUtilTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2022-2023 Google LLC
+ * Copyright 2013-2021 CompilerWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.application.dumper.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.edwmigration.dumper.application.dumper.utils.MemoizationUtil;
+import java.io.IOException;
+import org.apache.commons.io.function.IOFunction;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class MemoizationUtilTest {
+
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Mock private IOFunction<String, Integer> delegateIoFunction;
+
+  @Test
+  public void memoization_success() throws Exception {
+    when(delegateIoFunction.apply("input")).thenReturn(42);
+    IOFunction<String, Integer> underTest = MemoizationUtil.createMemoizer(delegateIoFunction);
+
+    // Act
+    Integer result1 = underTest.apply("input");
+    Integer result2 = underTest.apply("input");
+
+    // Verify
+    assertEquals(Integer.valueOf(42), result1);
+    assertEquals(Integer.valueOf(42), result2);
+    verify(delegateIoFunction).apply("input");
+    verifyNoMoreInteractions(delegateIoFunction);
+  }
+
+  @Test
+  public void memoization_ioExceptionGetsPropagated() throws Exception {
+    when(delegateIoFunction.apply("input")).thenThrow(new IOException("test IO error"));
+    IOFunction<String, Integer> underTest = MemoizationUtil.createMemoizer(delegateIoFunction);
+
+    // Act
+    IOException exception = assertThrows(IOException.class, () -> underTest.apply("input"));
+
+    // Verify
+    assertEquals(exception.getMessage(), "test IO error");
+  }
+
+  @Test
+  public void memoization_runtimeExceptionGetsPropagated() throws Exception {
+    when(delegateIoFunction.apply("input"))
+        .thenThrow(new IllegalStateException("test illegal state error"));
+    IOFunction<String, Integer> underTest = MemoizationUtil.createMemoizer(delegateIoFunction);
+
+    // Act
+    RuntimeException exception =
+        assertThrows(RuntimeException.class, () -> underTest.apply("input"));
+
+    // Verify
+    assertEquals(exception.getMessage(), "test illegal state error");
+    assertEquals(exception.getClass(), IllegalStateException.class);
+  }
+}
