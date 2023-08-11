097696a4ecedc2012d6e15665a70730188779adb
Christian Haselbach <haselbach@google.com>
2023-08-10 17:30:37 -0700

    Ensure licenses are retrieved from POM/manifest (#276)
    The CSV license report renderer from the JK1 license plugin collects
    licenses for a project in a list in the following order:
    
    1. License information from the POM files.
    2. License information from manifest files.
    3. Deducing the license by looking at the contents of license files.
    
    It then uses the last entry from that list. If there is license
    information from a POM or metadata file, we should rather use that.
    The custom CSV reporter in the commit uses the first license information
    that it finds using the above steps.
    
    Furthermore, it allows overriding license information for a project.

---
+++ buildSrc/src/main/groovy/com/google/edwmigration/dumper/build/licensereport/CsvReportRenderer.groovy
@@ -0,0 +1,76 @@
+package com.google.edwmigration.dumper.build.licensereport
+
+import static com.github.jk1.license.render.LicenseDataCollector.singleModuleLicenseInfo
+
+import org.gradle.api.tasks.Input
+import com.github.jk1.license.License
+import com.github.jk1.license.LicenseReportExtension
+import com.github.jk1.license.ModuleData
+import com.github.jk1.license.ProjectData
+import com.github.jk1.license.render.ReportRenderer
+import org.apache.commons.csv.CSVPrinter
+import org.apache.commons.csv.CSVFormat
+
+import java.util.stream.Collectors
+
+/**
+ * Custom CSV reporter that gets the license from the POM or the manifest but
+ * does not try to deduce it from a LICENSE file.
+ *
+ * Project overrides can be provided for example to provide license URLs for
+ * projects where the license is not declared in a POM or manifest.
+ **/
+class CsvReportRenderer implements ReportRenderer {
+    String[] header = ["artifact", "project url", "module license", "module license url"]
+    CSVFormat csvFormat = CSVFormat.EXCEL.builder().setHeader(header).build()
+
+    @Input
+    String filename
+    @Input
+    /** Custom CSV reporter that gets the license from the POM or the manifest but does not try to deduce it from a LICENSE file. */
+    Map<String, Map<String, String>> projectInfoOverrides
+
+    CsvReportRenderer(String filename = 'licenses.csv', Map<String, Map<String, String>> projectInfoOverrides) {
+        this.filename = filename
+        this.projectInfoOverrides = projectInfoOverrides
+    }
+
+    @Override
+    void render(ProjectData projectData) {
+        List<Map<String, ?>> records = projectData.allDependencies.sort().stream().map {
+            String project = "${it.group}:${it.name}"
+            String artifact = "${it.group}:${it.name}:${it.version}"
+            Map<String, String> projectInfoOverride = projectInfoOverrides.getOrDefault(project, [:])
+            License license = getLicense(it)
+            String projectUrl = it.poms.find { it.projectUrl }?.projectUrl
+            [
+                artifact            : artifact,
+                "project url"       : projectInfoOverride.getOrDefault("projectUrl", projectUrl),
+                "module license"    : projectInfoOverride.getOrDefault("licenseName", license?.name),
+                "module license url": projectInfoOverride.getOrDefault("licenseUrl", license?.url),
+            ]
+        }.collect(Collectors.toList())
+
+        LicenseReportExtension config = projectData.project.licenseReport
+        FileWriter writer = new FileWriter(new File(config.outputDir, filename))
+        CSVPrinter csv = new CSVPrinter(writer, csvFormat)
+        try {
+            csv.printRecords(records.stream().map { record -> header.collect { record[it] } })
+        } finally {
+            csv.close()
+        }
+    }
+
+    License getLicense(ModuleData moduleData) {
+        License license = moduleData.poms.collectMany { it.licenses }.find()
+        if (license) {
+            return license
+        }
+        String licenseName = moduleData.manifests.find { it.license }?.license
+        String licenseUrl = moduleData.manifests.find { it.licenseUrl }?.licenseUrl
+        if (licenseName || licenseUrl) {
+            return new License(name: licenseName, url: licenseUrl)
+        }
+        return null
+    }
+}
