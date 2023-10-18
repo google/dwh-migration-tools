f5ae90cfa16903771a601aa2507937560202fbaa
Karthik Ravikumar <karthiknr@google.com>
2023-10-18 10:44:08 -0700

    Add Cloudera connector task for dumping services

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/AbstractClouderaTask.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/AbstractClouderaTask.java
@@ -4,17 +4,18 @@ import com.cloudera.api.swagger.ClustersResourceApi;
 import com.cloudera.api.swagger.client.ApiException;
 import com.cloudera.api.swagger.model.ApiClusterList;
 import com.google.edwmigration.dumper.application.dumper.task.AbstractTask;
+
 import javax.annotation.Nonnull;
 
 public abstract class AbstractClouderaTask extends AbstractTask<Void> {
 
-  public AbstractClouderaTask(String targetPath) {
-    super(targetPath);
-  }
+    public AbstractClouderaTask(String targetPath) {
+        super(targetPath);
+    }
 
-  @Nonnull
-  protected ApiClusterList getClusters(@Nonnull ClouderaHandle handle) throws ApiException {
-    ClustersResourceApi api = new ClustersResourceApi(handle.getClient());
-    return api.readClusters(null, null);
-  }
+    @Nonnull
+    protected ApiClusterList getClusters(@Nonnull ClouderaHandle handle) throws ApiException {
+        ClustersResourceApi api = new ClustersResourceApi(handle.getClient());
+        return api.readClusters(null, null);
+    }
 }
--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/ClouderaHostsTask.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/ClouderaHostsTask.java
@@ -1,35 +1,36 @@
 package com.google.edwmigration.dumper.application.dumper.connector.cloudera;
 
-import com.cloudera.api.swagger.AllHostsResourceApi;
 import com.cloudera.api.swagger.HostsResourceApi;
-import com.cloudera.api.swagger.client.ApiClient;
 import com.cloudera.api.swagger.model.ApiHost;
 import com.cloudera.api.swagger.model.ApiHostList;
 import com.google.common.io.ByteSink;
 import com.google.edwmigration.dumper.application.dumper.handle.Handle;
 import com.google.edwmigration.dumper.application.dumper.task.TaskRunContext;
-import com.google.edwmigration.dumper.plugin.lib.dumper.spi.CoreMetadataDumpFormat;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
+
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
-public class ClouderaHostsTask extends AbstractClouderaTask{
+public class ClouderaHostsTask extends AbstractClouderaTask {
 
-  public ClouderaHostsTask() {
-    super("hosts.json");
-  }
-
-  @CheckForNull
-  @Override
-  protected Void doRun(TaskRunContext context, @Nonnull ByteSink sink, @Nonnull Handle handle)
-      throws Exception {
-    ClouderaHandle h = (ClouderaHandle) handle;
-    HostsResourceApi api = new HostsResourceApi(h.getClient());
-    ApiHostList list = api.readHosts(null, null, null);
-    try (Writer writer = sink.asCharSink(StandardCharsets.UTF_8).openBufferedStream()) {
-      CoreMetadataDumpFormat.MAPPER.writeValue(writer, list);
+    public ClouderaHostsTask() {
+        super("hosts.json");
     }
-    return null;
-  }
+
+    @CheckForNull
+    @Override
+    protected Void doRun(TaskRunContext context, @Nonnull ByteSink sink, @Nonnull Handle handle)
+            throws Exception {
+        ClouderaHandle h = (ClouderaHandle) handle;
+        HostsResourceApi api = new HostsResourceApi(h.getClient());
+        ApiHostList apiHostList = api.readHosts(null, null, null);
+        try (Writer writer = sink.asCharSink(StandardCharsets.UTF_8).openBufferedStream()) {
+            for (ApiHost apiHost : apiHostList.getItems()) {
+                writer.write(ClouderaConnectorUtils.MAPPER.writeValueAsString(apiHost));
+                writer.write("\n");
+            }
+        }
+        return null;
+    }
 }
