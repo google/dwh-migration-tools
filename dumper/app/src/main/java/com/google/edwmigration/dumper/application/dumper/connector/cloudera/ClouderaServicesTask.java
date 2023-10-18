b58fc5e6359e30d9082ab39f915b7e8c0cdb78db
Karthik Ravikumar <karthiknr@google.com>
2023-10-18 10:54:33 -0700

    Format code

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/ClouderaServicesTask.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/ClouderaServicesTask.java
@@ -16,41 +16,46 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class ClouderaServicesTask extends AbstractClouderaTask{
+public class ClouderaServicesTask extends AbstractClouderaTask {
 
+  public ClouderaServicesTask() {
+    super("services.json");
+  }
 
-    public ClouderaServicesTask() {
-        super("services.json");
-    }
+  // It would be more optimal to combine all metrics that fall under a common parent into a task, so
+  // we avoid repeated requests to the same API. For example, HDFS usage can be combined under this
+  // task since it pretty much calls the same APIs.
+  @CheckForNull
+  @Override
+  protected Void doRun(TaskRunContext context, @Nonnull ByteSink sink, @Nonnull Handle handle)
+      throws Exception {
+    ClouderaHandle h = (ClouderaHandle) handle;
+    ApiClusterList clusters = getClusters(h);
+    ServicesResourceApi api = new ServicesResourceApi(h.getClient());
 
-    // It would be more optimal to combine all metrics that fall under a common parent into a task, so we avoid repeated requests to the same API.
-    // For example HDFS usage can be combined under this task since it pretty much calls the same APIs.
-    @CheckForNull
-    @Override
-    protected Void doRun(TaskRunContext context, @Nonnull ByteSink sink, @Nonnull Handle handle) throws Exception {
-        ClouderaHandle h = (ClouderaHandle) handle;
-        ApiClusterList clusters = getClusters(h);
-        ServicesResourceApi api = new ServicesResourceApi(h.getClient());
+    ImmutableList.Builder servicesBuilder = ImmutableList.builder();
+    ImmutableList.Builder yarnMetricsBuilder = ImmutableList.builder();
 
-        ImmutableList.Builder servicesBuilder = ImmutableList.builder();
-        ImmutableList.Builder yarnMetricsBuilder = ImmutableList.builder();
-
-        // TODO: Accept startDate as an input. Arbitrarily setting it to today minus 7
-        String startDate = LocalDateTime.now().minusDays(7).format(DateTimeFormatter.ISO_DATE);
-        try (Writer writer = sink.asCharSink(StandardCharsets.UTF_8).openBufferedStream()) {
-            for (ApiCluster cluster : clusters.getItems()) {
-                ApiServiceList services = api.readServices(cluster.getName(), null);
-                for (ApiService service : services.getItems()) {
-                    // Includes name and health of each service
-                    servicesBuilder.add(service);
-                    // TODO: Figure out whether we want to use POOL or USER for tenant type or both
-                    ApiYarnUtilization yarnUtilization = api.getYarnUtilization(cluster.getName(),service.getName(),null,null,startDate,null,null,null);
-                    yarnMetricsBuilder.add(yarnUtilization);
-                }
-            }
-            ImmutableMap result = ImmutableMap.of("services",servicesBuilder.build(),"yarnMetrics",yarnMetricsBuilder.build());
-            CoreMetadataDumpFormat.MAPPER.writeValue(writer, result);
+    // TODO: Accept startDate as an input. Arbitrarily setting it to today minus 7
+    String startDate = LocalDateTime.now().minusDays(7).format(DateTimeFormatter.ISO_DATE);
+    try (Writer writer = sink.asCharSink(StandardCharsets.UTF_8).openBufferedStream()) {
+      for (ApiCluster cluster : clusters.getItems()) {
+        ApiServiceList services = api.readServices(cluster.getName(), null);
+        for (ApiService service : services.getItems()) {
+          // Includes name and health of each service
+          servicesBuilder.add(service);
+          // TODO: Figure out whether we want to use POOL or USER for tenant type or both
+          ApiYarnUtilization yarnUtilization =
+              api.getYarnUtilization(
+                  cluster.getName(), service.getName(), null, null, startDate, null, null, null);
+          yarnMetricsBuilder.add(yarnUtilization);
         }
-        return null;
+      }
+      ImmutableMap result =
+          ImmutableMap.of(
+              "services", servicesBuilder.build(), "yarnMetrics", yarnMetricsBuilder.build());
+      CoreMetadataDumpFormat.MAPPER.writeValue(writer, result);
     }
+    return null;
+  }
 }
