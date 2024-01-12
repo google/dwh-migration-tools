e4012d2673d74850d80ab84b2aacda1df72e629f
Kamil Piotrowski <kpiotrowski@google.com>
2024-01-12 15:44:15 +0100

    Fix date format

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/redshift/RedshiftRawLogsConnector.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/redshift/RedshiftRawLogsConnector.java
@@ -258,7 +258,7 @@ public class RedshiftRawLogsConnector extends AbstractRedshiftConnector
       ImmutableList<MetricConfig> metrics,
       List<? super Task<?>> out) {
     DateTimeFormatter dateFormat =
-        DateTimeFormatter.ofPattern("yyyy-MM-ddTHHmmssZ").withZone(ZoneOffset.UTC);
+        DateTimeFormatter.ofPattern("yyyy-MM-dd_HHmmss").withZone(ZoneOffset.UTC);
 
     AbstractAwsApiTask.createCredentialsProvider(arguments)
         .ifPresent(
