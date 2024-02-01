374b4afb808f37333d4e197943948dd750d4f75e
Kamil Piotrowski <kpiotrowski@google.com>
2024-02-01 09:52:39 +0100

    Add unit test

--- dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/teradata/TeradataLogsJdbcTaskTest.java
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/teradata/TeradataLogsJdbcTaskTest.java
@@ -115,4 +115,33 @@ public class TeradataLogsJdbcTaskTest {
             + " L.StartTime < CAST('2023-03-04T17:00:00Z' AS TIMESTAMP) AND L.UserName <> 'DBC'",
         query);
   }
+
+  @Test
+  public void toString_withZoneOffset_success() {
+    ZonedInterval interval =
+        new ZonedInterval(
+            ZonedDateTime.of(2023, 3, 4, 16, 0, 0, 0, ZoneOffset.ofHours(3)),
+            ZonedDateTime.of(2023, 3, 4, 17, 0, 0, 0, ZoneOffset.ofHours(3)));
+    TeradataLogsJdbcTask jdbcTask =
+        new TeradataLogsJdbcTask(
+            "result.csv",
+            queryLogsState,
+            QueryLogTableNames.create(
+                "SampleQueryTable", "SampleSqlTable", /* usingAtLeastOneAlternate */ true),
+            /* conditions= */ ImmutableSet.of(),
+            interval);
+    String unused = jdbcTask.getOrCreateSql(s -> true, ImmutableList.of("L.QueryID", "ST.QueryID"));
+
+    // Act
+    String taskDescription = jdbcTask.toString();
+
+    // Assert
+    assertEquals(
+        "Write result.csv from\n        SELECT L.QueryID, ST.QueryID"
+            + " FROM SampleQueryTable L LEFT OUTER JOIN SampleSqlTable ST ON (L.QueryID=ST.QueryID)"
+            + " WHERE L.ErrorCode=0 AND"
+            + " L.StartTime >= CAST('2023-03-04T13:00:00Z' AS TIMESTAMP) AND"
+            + " L.StartTime < CAST('2023-03-04T14:00:00Z' AS TIMESTAMP)",
+        taskDescription);
+  }
 }
