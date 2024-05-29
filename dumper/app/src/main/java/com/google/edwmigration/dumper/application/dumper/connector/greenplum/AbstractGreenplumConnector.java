22d05e03bbccdb473d418418ee301bcaace954f0
zzzzwang <80424912+zzzzwang@users.noreply.github.com>
2024-05-28 23:21:53 -0700

    Add metadata and log connector for greenplum (#392)
    * Add metadata and log connector for greenplum

---
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/greenplum/AbstractGreenplumConnector.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022-2024 Google LLC
+ * Copyright 2013-2021 CompilerWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.application.dumper.connector.greenplum;
+
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentDriver;
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentDriverClass;
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentHostUnlessUrl;
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentPassword;
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentUri;
+import com.google.edwmigration.dumper.application.dumper.annotations.RespectsArgumentUser;
+import com.google.edwmigration.dumper.application.dumper.connector.postgresql.AbstractPostgresqlConnector;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import javax.annotation.Nonnull;
+
+/** @author zzwang */
+@RespectsArgumentDriver
+@RespectsArgumentDriverClass
+@RespectsArgumentHostUnlessUrl
+@RespectsArgumentUser
+@RespectsArgumentPassword
+@RespectsArgumentUri
+public abstract class AbstractGreenplumConnector extends AbstractPostgresqlConnector {
+
+  protected static final DateTimeFormatter SQL_FORMAT =
+      DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneOffset.UTC);
+
+  public AbstractGreenplumConnector(String name) {
+    super(name);
+  }
+
+  @Nonnull
+  protected static CharSequence newWhereClause(String... clauseArray) {
+    StringBuilder buf = new StringBuilder();
+    for (String clause : clauseArray) {
+      if (buf.length() > 0) buf.append(" AND ");
+      buf.append(clause);
+    }
+    return buf.toString();
+  }
+}
---
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/greenplum/GreenplumMetadataTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022-2024 Google LLC
+ * Copyright 2013-2021 CompilerWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.application.dumper.connector.greenplum;
+
+import com.google.edwmigration.dumper.application.dumper.connector.AbstractConnectorExecutionTest;
+import com.google.edwmigration.dumper.application.dumper.connector.MetadataConnector;
+import com.google.edwmigration.dumper.test.TestUtils;
+import java.io.File;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Greenplum metadata connector tests */
+@RunWith(JUnit4.class)
+public class GreenplumMetadataTest extends AbstractConnectorExecutionTest {
+
+  @SuppressWarnings("UnusedVariable")
+  private static final Logger LOG = LoggerFactory.getLogger(GreenplumMetadataTest.class);
+
+  private static final String SUBPROJECT = "compilerworks-application-dumper";
+
+  private static final File PG_DUMPER_TEST =
+      new File(TestUtils.getTestResourcesDir(SUBPROJECT), "dumper-test/postgresql.sql");
+
+  private final MetadataConnector connector = new GreenplumMetadataConnector();
+
+  @Test
+  public void testConnector() throws Exception {
+    testConnectorDefaults(connector);
+  }
+}
