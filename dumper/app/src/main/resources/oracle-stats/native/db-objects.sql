539f7276271a1682dfeae5ceaee4fa550a738f0d
Mikołaj Sołtan <misolt@google.com>
2024-06-13 08:27:35 +0000

    fix indentation and aliases

--- dumper/app/src/main/resources/oracle-stats/native/db-objects.sql
+++ dumper/app/src/main/resources/oracle-stats/native/db-objects.sql
@@ -12,36 +12,46 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-SELECT con_id ,
-       owner ,
-       object_type ,
-       editionable ,
-       count
+SELECT
+  con_id ,
+  owner ,
+  object_type ,
+  editionable ,
+  count
 FROM (
-        SELECT i.con_id,
-               i.owner,
-               i.object_type,
-               i.editionable,
-               COUNT(1)              count
-        FROM
-        (
-        SELECT
-               con_id,
-               owner,
-               object_type,
-               editionable AS editionable,
-               object_name
-        FROM cdb_objects a
-        WHERE  (owner = 'SYS' AND object_type = 'DIRECTORY')
-           OR owner NOT LIKE '%SYS'
-) i
-        LEFT OUTER JOIN
-        (
-        SELECT 'SYNONYM' as object_type, owner, synonym_name, con_id, table_owner
-        FROM cdb_synonyms b
-        WHERE owner = 'PUBLIC') x
-        ON i.object_type = x.object_type AND i.owner = x.owner AND i.object_name = x.synonym_name AND i.con_id = x.con_id
-        WHERE (CASE WHEN i.object_type = 'SYNONYM' and i.owner ='PUBLIC' and ( i.object_name like '/%' OR x.table_owner IS NOT NULL) THEN 0 ELSE 1 END = 1)
-        AND i.object_name NOT LIKE 'BIN$%'
-        GROUP  BY  i.con_id, i.owner, i.editionable , i.object_type
+  SELECT
+    B.con_id,
+    B.owner,
+    B.object_type,
+    B.editionable,
+    COUNT(1) count
+  FROM (
+    SELECT
+      A.con_id,
+      A.owner,
+      A.object_type,
+      A.editionable,
+      A.object_name
+    FROM cdb_objects A
+    WHERE  (A.owner = 'SYS' AND A.object_type = 'DIRECTORY')
+      OR A.owner NOT LIKE '%SYS'
+  ) B
+  LEFT OUTER JOIN (
+    SELECT
+      'SYNONYM' object_type,
+      C.owner, synonym_name,
+      C.con_id,
+      C.table_owner
+    FROM cdb_synonyms C
+    WHERE C.owner = 'PUBLIC'
+  ) D ON B.object_type = D.object_type
+    AND B.owner = D.owner
+    AND B.object_name = D.synonym_name
+    AND B.con_id = D.con_id
+  WHERE (
+    CASE WHEN B.object_type = 'SYNONYM' and B.owner ='PUBLIC' and ( B.object_name like '/%' OR D.table_owner IS NOT NULL
+    ) THEN 0 ELSE 1 END
+    = 1)
+  AND B.object_name NOT LIKE 'BIN$%'
+  GROUP  BY  B.con_id, B.owner, B.editionable , B.object_type
 )
