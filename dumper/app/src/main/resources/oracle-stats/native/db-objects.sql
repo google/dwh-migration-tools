a07c46af7f7838151b49a8a993be4a552b632abb
Mikołaj Sołtan <misolt@google.com>
2024-06-13 11:09:33 +0000

    remove a redundant subquery

--- dumper/app/src/main/resources/oracle-stats/native/db-objects.sql
+++ dumper/app/src/main/resources/oracle-stats/native/db-objects.sql
@@ -13,45 +13,37 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 SELECT
-  con_id ,
-  owner ,
-  object_type ,
-  editionable ,
-  count
+  B.con_id,
+  B.owner,
+  B.object_type,
+  B.editionable,
+  COUNT(1) count
 FROM (
   SELECT
-    B.con_id,
-    B.owner,
-    B.object_type,
-    B.editionable,
-    COUNT(1) count
-  FROM (
-    SELECT
-      A.con_id,
-      A.owner,
-      A.object_type,
-      A.editionable,
-      A.object_name
-    FROM cdb_objects A
-    WHERE  (A.owner = 'SYS' AND A.object_type = 'DIRECTORY')
-      OR A.owner NOT LIKE '%SYS'
-  ) B
-  LEFT OUTER JOIN (
-    SELECT
-      C.owner, synonym_name,
-      C.con_id,
-      C.table_owner
-    FROM cdb_synonyms C
-    WHERE C.owner = 'PUBLIC'
-  ) D ON B.object_type = 'SYNONYM'
-    AND B.owner = D.owner
-    AND B.object_name = D.synonym_name
-    AND B.con_id = D.con_id
-  WHERE (
-      B.object_type <> 'SYNONYM'
-      OR B.owner <> 'PUBLIC'
-      OR (B.object_name NOT LIKE '/%' AND D.table_owner IS NULL)
-    )
-    AND B.object_name NOT LIKE 'BIN$%'
-  GROUP  BY  B.con_id, B.owner, B.editionable , B.object_type
-)
+    A.con_id,
+    A.owner,
+    A.object_type,
+    A.editionable,
+    A.object_name
+  FROM cdb_objects A
+  WHERE  (A.owner = 'SYS' AND A.object_type = 'DIRECTORY')
+    OR A.owner NOT LIKE '%SYS'
+) B
+LEFT OUTER JOIN (
+  SELECT
+    C.owner, synonym_name,
+    C.con_id,
+    C.table_owner
+  FROM cdb_synonyms C
+  WHERE C.owner = 'PUBLIC'
+) D ON B.object_type = 'SYNONYM'
+  AND B.owner = D.owner
+  AND B.object_name = D.synonym_name
+  AND B.con_id = D.con_id
+WHERE (
+    B.object_type <> 'SYNONYM'
+    OR B.owner <> 'PUBLIC'
+    OR (B.object_name NOT LIKE '/%' AND D.table_owner IS NULL)
+  )
+  AND B.object_name NOT LIKE 'BIN$%'
+GROUP  BY  B.con_id, B.owner, B.editionable , B.object_type
