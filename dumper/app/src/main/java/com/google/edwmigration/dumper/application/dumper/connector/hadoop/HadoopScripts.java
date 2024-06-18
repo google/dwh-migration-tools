289caaac99685fdcaddc2a26ff8cd29831647c9c
Dawid Chodura <dchodura@google.com>
2024-06-19 00:33:14 +0200

    [b/341097448] Use temporary directory for extracting scripts (#427)
    Use temporary directory for extracting scripts, remove the commons-exec dependency.

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/hadoop/HadoopScripts.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/hadoop/HadoopScripts.java
@@ -16,13 +16,38 @@
  */
 package com.google.edwmigration.dumper.application.dumper.connector.hadoop;
 
-import com.google.common.io.Files;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Suppliers.memoize;
+
 import com.google.common.io.Resources;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class HadoopScripts {
+  private static final Logger LOG = LoggerFactory.getLogger(HadoopScripts.class);
+  private static final String TMP_DIR_PREFIX = "dwh-migration-tools-tmp-";
+  private static final Supplier<Path> SCRIPT_DIR_SUPPLIER =
+      memoize(
+          () -> {
+            Path tmpDir;
+            Path workingDir = Paths.get("");
+            LOG.info(
+                "Creating temporary directory in the working directory '{}'...",
+                workingDir.toAbsolutePath());
+            try {
+              tmpDir = Files.createTempDirectory(workingDir, TMP_DIR_PREFIX);
+            } catch (IOException e) {
+              throw new IllegalStateException("Error creating temporary directory.", e);
+            }
+            LOG.info("Temporary directory created: '{}'.", tmpDir);
+            return tmpDir;
+          });
 
   /** Reads the script from the resources directory inside the jar. */
   static byte[] read(String scriptFilename) throws IOException {
@@ -31,13 +56,17 @@ class HadoopScripts {
   }
 
   /** Extracts the script from the resources directory inside the jar to the local filesystem. */
-  static File extract(String scriptFilename) throws IOException {
+  static Path extract(String scriptFilename) throws IOException {
     byte[] scriptBody = HadoopScripts.read(scriptFilename);
-    File scriptDir = new File("dwh-migration-tools-tmp");
-    scriptDir.mkdirs();
-    File scriptFile = new File(scriptDir, scriptFilename);
-    Files.write(scriptBody, scriptFile);
-    scriptFile.setExecutable(true);
-    return scriptFile;
+    Path scriptDir = SCRIPT_DIR_SUPPLIER.get();
+    checkState(
+        Files.exists(scriptDir) && Files.isDirectory(scriptDir),
+        "Directory '%s' does not exist.",
+        scriptDir);
+    LOG.info("Extracting '{}' to '{}'...", scriptFilename, scriptDir);
+    Path scriptPath = scriptDir.resolve(scriptFilename);
+    Files.write(scriptPath, scriptBody);
+    scriptPath.toFile().setExecutable(true);
+    return scriptPath;
   }
 }
