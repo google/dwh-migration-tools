03c6c8e7ce338ad96e929305b6c99c8de5695ad9
Mikołaj Sołtan <misolt@google.com>
2024-07-01 14:30:58 +0000

    shorten the name to exttab

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
@@ -47,7 +47,7 @@ class StatsTaskListGenerator {
           // A JOIN is performed to exclude objects which appear in the cdb_synonyms table.
           "db-objects-synonym-public",
           "dtl-source-code",
-          "external-tables",
+          "exttab",
           "m-view-types",
           "pdbs-info",
           "app-schemas-pdbs",
