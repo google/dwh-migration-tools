1bc249dea73b712d9f3a972d5db6af864db4cf01
Mikołaj Sołtan <misolt@google.com>
2024-07-16 09:13:22 +0000

    add tests

--- dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/OracleStatsConnectorTest.java
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/OracleStatsConnectorTest.java
@@ -17,17 +17,55 @@
 package com.google.edwmigration.dumper.application.dumper.connector.oracle;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
 
+import com.google.edwmigration.dumper.application.dumper.ConnectorArguments;
+import com.google.edwmigration.dumper.application.dumper.MetadataDumperUsageException;
+import java.io.IOException;
 import org.junit.Test;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(Theories.class)
 public class OracleStatsConnectorTest {
 
   @Test
   public void getConnectorScope_success() {
     OracleStatsConnector connector = new OracleStatsConnector();
     assertEquals(OracleConnectorScope.STATS, connector.getConnectorScope());
+  }
+
+  enum LogTime {
+    SHORT(7),
+    LONG(30);
+
+    final int asDays;
+
+    LogTime(int asDays) {
+      this.asDays = asDays;
+    }
+  }
+
+  @Theory
+  public void getQueryLogDays_success(LogTime time) throws IOException {
+    ConnectorArguments arguments =
+        new ConnectorArguments(
+            "--connector", "oracle-stats", "--query-log-days", String.valueOf(time.asDays));
+    int days = OracleStatsConnector.getQueryLogDays(arguments);
+    assertEquals(time.asDays, days);
+  }
+
+  @Test
+  public void getQueryLogDays_wrongNumberOfDays_throwsException() throws IOException {
+    ConnectorArguments arguments =
+        new ConnectorArguments("--connector", "oracle-stats", "--query-log-days", "999");
+    MetadataDumperUsageException exception =
+        assertThrows(
+            MetadataDumperUsageException.class,
+            () -> OracleStatsConnector.getQueryLogDays(arguments));
+    assertTrue(exception.getMessage().contains("999"));
+    assertTrue(exception.getMessage().startsWith("The number of days must be either 7 or 30"));
   }
 }
