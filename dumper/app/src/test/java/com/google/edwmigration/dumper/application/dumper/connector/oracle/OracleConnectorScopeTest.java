1b27c718c8b690c632706e5e99b975fa0c684a6e
Dawid Chodura <dchodura@google.com>
2024-07-30 21:25:45 +0000

    Make OracleConnectorScopeTest parameterized

--- dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/OracleConnectorScopeTest.java
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/OracleConnectorScopeTest.java
@@ -16,50 +16,78 @@
  */
 package com.google.edwmigration.dumper.application.dumper.connector.oracle;
 
-import static com.google.edwmigration.dumper.application.dumper.connector.oracle.OracleConnectorScope.*;
+import static com.google.edwmigration.dumper.application.dumper.connector.oracle.OracleConnectorScope.LOGS;
+import static com.google.edwmigration.dumper.application.dumper.connector.oracle.OracleConnectorScope.METADATA;
+import static com.google.edwmigration.dumper.application.dumper.connector.oracle.OracleConnectorScope.STATS;
 import static java.time.ZoneOffset.UTC;
 import static org.junit.Assert.assertEquals;
 
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
 import java.time.Clock;
 import java.time.Instant;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(Theories.class)
 public class OracleConnectorScopeTest {
+  Instant instant = Instant.ofEpochMilli(1715346130945L);
+  Clock clock = Clock.fixed(instant, UTC);
 
   @Test
-  public void toDisplayName_success() {
+  public void connectorName_success() {
     assertEquals("oracle-logs", LOGS.connectorName());
     assertEquals("oracle", METADATA.connectorName());
     assertEquals("oracle-stats", STATS.connectorName());
   }
 
   @Test
-  public void toFormat_success() {
+  public void formatName_success() {
     assertEquals("oracle.logs.zip", LOGS.formatName());
     assertEquals("oracle.dump.zip", METADATA.formatName());
     assertEquals("oracle.stats.zip", STATS.formatName());
   }
 
-  @Test
-  public void toFileName_forAssessment_success() {
-    Instant instant = Instant.ofEpochMilli(1715346130945L);
-    Clock clock = Clock.fixed(instant, UTC);
+  @DataPoints("assessment")
+  public static ImmutableList<TestCase> ASSESSMENT_TEST_CASES =
+      ImmutableList.of(
+          TestCase.create(LOGS, "dwh-migration-oracle-logs-20240510T130210.zip"),
+          TestCase.create(METADATA, "dwh-migration-oracle-metadata.zip"),
+          TestCase.create(STATS, "dwh-migration-oracle-stats.zip"));
 
-    assertEquals("dwh-migration-oracle-logs-20240510T130210.zip", LOGS.toFileName(true, clock));
-    assertEquals("dwh-migration-oracle-metadata.zip", METADATA.toFileName(true, clock));
-    assertEquals("dwh-migration-oracle-stats.zip", STATS.toFileName(true, clock));
+  @Theory
+  public void toFileName_forAssessment_success(@FromDataPoints("assessment") TestCase testCase) {
+    String filename = testCase.oracleConnectorScope().toFileName(/* isAssessment= */ true, clock);
+    assertEquals(testCase.expectedFilename(), filename);
   }
 
-  @Test
-  public void toFileName_notForAssessment_success() {
-    Instant instant = Instant.ofEpochMilli(1715346130945L);
-    Clock clock = Clock.fixed(instant, UTC);
+  @DataPoints("nonAssessment")
+  public static ImmutableList<TestCase> NON_ASSESSMENT_TEST_CASES =
+      ImmutableList.of(
+          TestCase.create(LOGS, "dwh-migration-oracle-logs.zip"),
+          TestCase.create(METADATA, "dwh-migration-oracle-metadata.zip"),
+          TestCase.create(STATS, "dwh-migration-oracle-stats.zip"));
 
-    assertEquals("dwh-migration-oracle-logs.zip", LOGS.toFileName(false, clock));
-    assertEquals("dwh-migration-oracle-metadata.zip", METADATA.toFileName(false, clock));
-    assertEquals("dwh-migration-oracle-stats.zip", STATS.toFileName(false, clock));
+  @Theory
+  public void toFileName_notForAssessment_success(
+      @FromDataPoints("nonAssessment") TestCase testCase) {
+    String filename = testCase.oracleConnectorScope().toFileName(/* isAssessment= */ false, clock);
+    assertEquals(testCase.expectedFilename(), filename);
+  }
+
+  @AutoValue
+  abstract static class TestCase {
+    abstract OracleConnectorScope oracleConnectorScope();
+
+    abstract String expectedFilename();
+
+    static TestCase create(OracleConnectorScope oracleConnectorScope, String expectedFilename) {
+      return new AutoValue_OracleConnectorScopeTest_TestCase(
+          oracleConnectorScope, expectedFilename);
+    }
   }
 }
