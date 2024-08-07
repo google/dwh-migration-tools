7eca97c19ad0fd0f2f87778781c4ad8e24b7305f
Mikołaj Sołtan <misolt@google.com>
2024-08-07 09:41:31 +0000

    remove app-schemas-summary

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
@@ -46,7 +46,6 @@ class StatsTaskListGenerator {
 
   private static final ImmutableList<String> NATIVE_NAMES_OPTIONAL_CDB_ONLY =
       ImmutableList.of(
-          "app-schemas-summary",
           "db-instances",
           "dtl-index-type",
           "dtl-source-code",
