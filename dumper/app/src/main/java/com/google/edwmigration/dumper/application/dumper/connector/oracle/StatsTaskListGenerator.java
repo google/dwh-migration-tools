98868b63accea9e0b626b750de632c1743aaaf6c
Mikołaj Sołtan <misolt@google.com>
2024-08-08 10:42:00 +0000

    Add non-CDB version of exttab SQL

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/oracle/StatsTaskListGenerator.java
@@ -43,10 +43,10 @@ class StatsTaskListGenerator {
       ImmutableList.of("hist-cmd-types-awr", "source-conn-latest", "sql-stats-awr");
 
   private static final ImmutableList<String> NATIVE_NAMES_OPTIONAL =
-      ImmutableList.of("app-schemas-pdbs", "db-features", "db-instances", "dtl-index-type");
+      ImmutableList.of("app-schemas-pdbs", "db-features", "db-instances", "dtl-index-type", "exttab");
 
   private static final ImmutableList<String> NATIVE_NAMES_OPTIONAL_CDB_ONLY =
-      ImmutableList.of("dtl-source-code", "exttab", "m-view-types", "pdbs-info");
+      ImmutableList.of("dtl-source-code", "m-view-types", "pdbs-info");
 
   private static final ImmutableList<String> NATIVE_NAMES_REQUIRED =
       ImmutableList.of(
