52472dcee13914f24de02b3ef002a875171661ed
Dawid Chodura <dchodura@google.com>
2024-08-23 14:26:44 +0200

    [b/361574176] Extract additional software packages (#523)

--- dumper/app/src/main/resources/hadoop-scripts/apt-list-packages.sh
+++ dumper/app/src/main/resources/hadoop-scripts/apt-list-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if command -v apt > /dev/null 2>&1 ; then
-  apt list --installed | grep "^\(docker\|g++\|gcc\|hadoop\|hbase\|hive\|mysql\|oozie\|perl\|pig\|python\|r-\|ranger\|ruby\|scala\|spark\|sqoop\|tez/\|zookeeper\)"
+  apt list --installed | grep "^\(grafana\|knox\|ganglia\|omd-\|sysstat\|cron\|docker\|g++\|gcc\|hadoop\|hbase\|hive\|mysql\|oozie\|perl\|pig\|python\|r-\|ranger\|ruby\|scala\|spark\|sqoop\|tez/\|zookeeper\)"
 fi
