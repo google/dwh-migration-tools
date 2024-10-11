5b054f00684f92c4f7cf6bb74c6321b940c99411
Vladislav Sidorovich <vsidorovich@google.com>
2024-10-11 10:25:02 +0200

    fix spotless codestyle (#585)

--- dumper/lib-ext-bigquery/src/main/java/com/google/edwmigration/dumper/plugin/ext/bigquery/BigQueryClientUtils.java
+++ dumper/lib-ext-bigquery/src/main/java/com/google/edwmigration/dumper/plugin/ext/bigquery/BigQueryClientUtils.java
@@ -30,9 +30,7 @@ import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author shevek
- */
+/** @author shevek */
 public class BigQueryClientUtils {
 
   @SuppressWarnings("UnusedVariable")
