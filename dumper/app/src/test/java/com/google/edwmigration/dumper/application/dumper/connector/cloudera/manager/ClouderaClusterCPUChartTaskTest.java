abccc22065a02903067a1b528a101c25028b4035
Denys Zaluzhnyi <dzaluzhnyi@google.com>
2024-12-03 17:30:17 +0100

    Added new test file

---
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/manager/ClouderaClusterCPUChartTaskTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022-2024 Google LLC
+ * Copyright 2013-2021 CompilerWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.edwmigration.dumper.application.dumper.connector.cloudera.manager;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyChar;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteSink;
+import com.google.common.io.CharSink;
+import com.google.edwmigration.dumper.application.dumper.connector.cloudera.manager.ClouderaManagerHandle.ClouderaClusterDTO;
+import com.google.edwmigration.dumper.application.dumper.task.TaskRunContext;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClouderaClusterCPUChartTaskTest {
+  private final ClouderaClusterCPUChartTask task = new ClouderaClusterCPUChartTask();
+  private ClouderaManagerHandle handle;
+
+  @Mock private TaskRunContext context;
+  @Mock private ByteSink sink;
+  @Mock private Writer writer;
+  @Mock private CharSink charSink;
+  @Mock private CloseableHttpClient httpClient;
+  private URI uri;
+
+  @Before
+  public void setUp() throws Exception {
+    uri = URI.create("http://localhost/");
+    handle = new ClouderaManagerHandle(uri, httpClient);
+
+    when(sink.asCharSink(eq(StandardCharsets.UTF_8))).thenReturn(charSink);
+    when(charSink.openBufferedStream()).thenReturn(writer);
+  }
+
+  @Test
+  public void noClusterId_skipWrites() throws Exception {
+    // GIVEN: There is no cluster with a valid cluster ID
+    handle.initClusters(ImmutableList.of(ClouderaClusterDTO.create(null, "single cluster")));
+
+    // WHEN
+    task.doRun(context, sink, handle);
+
+    // THEN: Task for such clusters should be skipped
+    verify(httpClient, never()).execute(any());
+    verifyNoWrites();
+  }
+
+  private void verifyNoWrites() throws IOException {
+    verify(writer, never()).write(anyChar());
+    verify(writer, never()).write(anyString());
+    verify(writer, never()).write(anyString(), anyInt(), anyInt());
+    verify(writer, never()).write(any(char[].class));
+    verify(writer, never()).write(any(char[].class), anyInt(), anyInt());
+  }
+}
