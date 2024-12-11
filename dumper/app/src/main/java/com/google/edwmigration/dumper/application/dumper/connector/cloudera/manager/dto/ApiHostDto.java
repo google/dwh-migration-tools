a58737d5f197fac0fe48316c6a106d3a9888ab8d
Denys Zaluzhnyi <dzaluzhnyi@google.com>
2024-12-11 14:17:51 +0100

    Add doc strings for DTOs

--- dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/manager/dto/ApiHostDto.java
+++ dumper/app/src/main/java/com/google/edwmigration/dumper/application/dumper/connector/cloudera/manager/dto/ApiHostDto.java
@@ -19,6 +19,14 @@ package com.google.edwmigration.dumper.application.dumper.connector.cloudera.man
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * DTO class for the official Cloudera API <a
+ * href="https://archive.cloudera.com/cm7/7.11.3.0/generic/jar/cm_api/apidocs/json_ApiHost.html">json</a>
+ *
+ * <p>Note: The license for <a
+ * href="https://mvnrepository.com/artifact/com.cloudera.api.swagger/cloudera-manager-api-swagger/7.11.0">generated</a>
+ * code is unclear, the own model for public schema used instead of it.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApiHostDto {
 
