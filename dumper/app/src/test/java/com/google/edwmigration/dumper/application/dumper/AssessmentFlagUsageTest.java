4d69e1e286b91ecd7665f928ed14a341e3cc8db3
Vladislav Sidorovich <vsidorovich@google.com>
2025-03-20 14:43:22 +0100

    use annotation util to cover parent classes

--- dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/AssessmentFlagUsageTest.java
+++ dumper/app/src/test/java/com/google/edwmigration/dumper/application/dumper/AssessmentFlagUsageTest.java
@@ -8,6 +8,7 @@ import com.google.edwmigration.dumper.application.dumper.connector.Connector;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
 
 public class AssessmentFlagUsageTest {
   private static final Logger logger = LoggerFactory.getLogger(AssessmentFlagUsageTest.class);
@@ -16,8 +17,8 @@ public class AssessmentFlagUsageTest {
   public void assessmentFlagIsExplicitlySpecified() throws Exception {
     ImmutableCollection<Connector> connectors = ConnectorRepository.getInstance().getAllConnectors();
     for (Connector connector : connectors) {
-      RespectsArgumentAssessment assessment = connector.getClass()
-          .getDeclaredAnnotation(RespectsArgumentAssessment.class);
+      RespectsArgumentAssessment assessment = AnnotationUtils.findAnnotation(connector.getClass(),
+          RespectsArgumentAssessment.class);
       if (assessment != null) {
         assertThrows(
             Exception.class,
