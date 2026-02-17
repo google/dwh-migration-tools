487bacd862ca06cef97866a6b0cc3ee3e958abaa
Kakha Philauri <45598419+kaxuna@users.noreply.github.com>
2026-02-17 14:39:05 +0100

    added Function_ID to UDF csv (#1091)

--- dumper/lib-dumper-spi/src/main/java/com/google/edwmigration/dumper/plugin/lib/dumper/spi/SnowflakeMetadataDumpFormat.java
+++ dumper/lib-dumper-spi/src/main/java/com/google/edwmigration/dumper/plugin/lib/dumper/spi/SnowflakeMetadataDumpFormat.java
@@ -112,6 +112,7 @@ public interface SnowflakeMetadataDumpFormat {
     String IS_ZIP_ENTRY_NAME = "user_defined_functions.csv";
 
     enum Header {
+      FUNCTION_ID,
       FUNCTION_CATALOG,
       FUNCTION_SCHEMA,
       FUNCTION_NAME,
